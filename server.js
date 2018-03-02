c3500b00f99f41880c72b7154698869a90f05130
Shobha <shobha020@gmail.com>
2018-03-01 19:54:54 -0800

    added node.js app

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+http.createServer(function(request,response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello, testing nodejs app');
+
+
+}).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/')
+
